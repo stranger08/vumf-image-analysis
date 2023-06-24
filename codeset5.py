6c6cd7b47c25b970a26999ed1d8f9e771b412c26
stranger08 <ramanauskassaras@gmail.com>
2023-06-24 12:02:46 +0000

    contraharmonic filter added

--- codeset5.py
+++ codeset5.py
@@ -37,6 +37,39 @@ def geometricMeanFilter(img, m, n, mirror=False):
       out[img_i, img_j] = acc**(1/(m*n))
   return out
 
+def contraharmonicMeanFilter(img, m, n, q, mirror=False):
+  out = np.copy(img)
+  mCenter = m // 2
+  nCenter = n // 2
+
+  for img_i, row in enumerate(img):
+    for img_j, x in enumerate(row):
+
+      accQ0 = 0
+      accQ1 = 0
+      for filterOffsetI in range(-mCenter, mCenter + 1):
+        for filterOffsetJ in range(-nCenter, nCenter + 1):
+          # calc eff coordinates
+          i = img_i + filterOffsetI
+          j = img_j + filterOffsetJ
+          # check if outside of an image
+          if i < 0 or i >= img.shape[0]:
+            if mirror:
+              i = img_i - filterOffsetI
+            else:
+              continue
+          if j < 0 or j >= img.shape[1]:
+            if mirror:
+              j = img_j - filterOffsetJ
+            else:
+              continue
+          accQ0 += np.power(img[i, j], q) if img[i, j] != 0 else 1
+          accQ1 += np.power(img[i, j], q + 1) if img[i, j] != 0 else 1
+
+      if accQ0 != 0:
+        out[img_i, img_j] = accQ1 / accQ0
+  return out
+
 def testGaussianNoiseRemoval():
   tifImg = TIFF.open(IMG_SET_5_REL_PATH + 'Fig0507(a)(ckt-board-orig).tif', mode='r')
   img = tifImg.read_image().astype(np.float64)
@@ -67,7 +100,37 @@ def testGaussianNoiseRemoval():
   plt.show()
   tifImg.close()
 
-testGaussianNoiseRemoval()
+#testGaussianNoiseRemoval()
 
 def testSaltNPepperNoiseRemoval():
-  pass
+  tifImgPepperNoise = TIFF.open(IMG_SET_5_REL_PATH + 'Fig0508(a)(circuit-board-pepper-prob-pt1).tif', mode='r')
+  imgPep = tifImgPepperNoise.read_image().astype(np.float64)
+
+  tifImgSaltNoise = TIFF.open(IMG_SET_5_REL_PATH + 'Fig0508(b)(circuit-board-salt-prob-pt1).tif', mode='r')
+  imgSalt = tifImgSaltNoise.read_image().astype(np.float64)
+
+  imgPepRemoved = contraharmonicMeanFilter(imgPep, 3, 3, 1.5, mirror=False)
+  imgPepRemoved8 = to8bit(imgPepRemoved, mode='scale')
+
+  imgSaltRemoved = contraharmonicMeanFilter(imgSalt, 3, 3, -1.5, mirror=False)
+  imgSaltRemoved8 = to8bit(imgSaltRemoved, mode='scale')
+
+  fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(16,9))
+  ax1.imshow(imgPep, cmap='gray', vmin=0, vmax=MAX_VAL)
+  ax1.set_axis_off()
+  ax1.set_title('Pepper noise')
+  ax2.imshow(imgSalt, cmap='gray', vmin=0, vmax=MAX_VAL)
+  ax2.set_axis_off()
+  ax2.set_title('Salt noise')
+  ax3.imshow(imgPepRemoved8, cmap='gray', vmin=0, vmax=MAX_VAL)
+  ax3.set_axis_off()
+  ax3.set_title('Pepper noise removed')
+  ax4.imshow(imgSaltRemoved8, cmap='gray', vmin=0, vmax=MAX_VAL)
+  ax4.set_axis_off()
+  ax4.set_title('Salt noise removed')
+  plt.tight_layout()
+  plt.show()
+  tifImgPepperNoise.close()
+  tifImgSaltNoise.close()
+
+testSaltNPepperNoiseRemoval()
